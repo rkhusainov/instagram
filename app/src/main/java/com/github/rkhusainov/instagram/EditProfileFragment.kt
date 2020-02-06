41a9b7165279eb3f366ccf059de6796487b68a47
Rinat Khusainov <rinat.khusainov.msk@gmail.com>
2020-02-06 14:42:16 +0300

    Create edit profile screen, add some customizations to style

---
+++ app/src/main/java/com/github/rkhusainov/instagram/EditProfileFragment.kt
@@ -0,0 +1,31 @@
+package com.github.rkhusainov.instagram
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_edit_profile.*
+
+class EditProfileFragment : Fragment() {
+    companion object {
+        fun newInstance(): EditProfileFragment {
+            return EditProfileFragment()
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_edit_profile, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        close_image.setOnClickListener {
+            fragmentManager?.popBackStack()
+        }
+    }
+}
--- app/src/main/java/com/github/rkhusainov/instagram/ProfileFragment.kt
+++ app/src/main/java/com/github/rkhusainov/instagram/ProfileFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment() {
 
@@ -20,5 +21,16 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        edit_profile_button.setOnClickListener {
+            fragmentManager
+                ?.beginTransaction()
+                ?.addToBackStack(null)
+                ?.replace(R.id.fragment_container, EditProfileFragment.newInstance())
+                ?.commit()
+        }
     }
 }
