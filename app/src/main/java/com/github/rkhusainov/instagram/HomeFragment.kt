af09feb39c017a70c2b8e9ce9d21c08db2ac7517
Rinat Khusainov <rinat.khusainov.msk@gmail.com>
2020-02-09 11:45:57 +0300

    Add authorization logic

--- app/src/main/java/com/github/rkhusainov/instagram/HomeFragment.kt
+++ app/src/main/java/com/github/rkhusainov/instagram/HomeFragment.kt
@@ -1,16 +1,27 @@
 package com.github.rkhusainov.instagram
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
+
+    private lateinit var callbackListener: CallbackListener
 
     companion object {
         fun newInstance(): HomeFragment {
             return HomeFragment()
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is CallbackListener) {
+            callbackListener = context
         }
     }
 
@@ -21,4 +32,15 @@ class HomeFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        sign_out_text.setOnClickListener {
+            callbackListener.signOut()
+        }
+    }
+}
+
+interface CallbackListener {
+    fun signOut()
 }
--- app/src/main/java/com/github/rkhusainov/instagram/LoginActivity.kt
+++ app/src/main/java/com/github/rkhusainov/instagram/LoginActivity.kt
@@ -1,15 +1,20 @@
 package com.github.rkhusainov.instagram
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEvent
 import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEventListener
 
 class LoginActivity : AppCompatActivity(), KeyboardVisibilityEventListener, TextWatcher {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +24,24 @@ class LoginActivity : AppCompatActivity(), KeyboardVisibilityEventListener, Text
         login_btn.isEnabled = false
         email_input.addTextChangedListener(this)
         password_input.addTextChangedListener(this)
+        auth = FirebaseAuth.getInstance()
+
+        login_btn.setOnClickListener {
+            val email = email_input.text.toString()
+            val password = password_input.text.toString()
+            if (validate(email, password)) {
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Please enter email and password", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
+
 
     override fun onVisibilityChanged(isKeyboardOpen: Boolean) {
         if (isKeyboardOpen) {
@@ -32,10 +54,12 @@ class LoginActivity : AppCompatActivity(), KeyboardVisibilityEventListener, Text
     }
 
     override fun afterTextChanged(s: Editable?) {
-        login_btn.isEnabled =
-            email_input.text.toString().isNotEmpty() &&
-                    password_input.text.toString().isNotEmpty()
+        login_btn.isEnabled = validate(email_input.text.toString(), password_input.text.toString())
+
     }
+
+    private fun validate(email: String, password: String) =
+        email.isNotEmpty() && password.isNotEmpty()
 
     override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
     }
