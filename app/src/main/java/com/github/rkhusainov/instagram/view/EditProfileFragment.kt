196bed57fcd47428b94162e2d1ab0b561d5cf9e1
Rinat Khusainov <rinat.khusainov.msk@gmail.com>
2020-02-12 16:31:34 +0300

    Taking and saving photo to the path and to the firebase storage

--- app/src/main/java/com/github/rkhusainov/instagram/view/EditProfileFragment.kt
+++ app/src/main/java/com/github/rkhusainov/instagram/view/EditProfileFragment.kt
@@ -1,13 +1,19 @@
 package com.github.rkhusainov.instagram.view
 
+import android.app.Activity.RESULT_OK
 import android.content.ContentValues.TAG
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.github.rkhusainov.instagram.R
@@ -15,14 +21,23 @@ import com.github.rkhusainov.instagram.model.User
 import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.fragment_edit_profile.*
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
 class EditProfileFragment : Fragment(), PasswordDialog.Listener {
 
+    private val TAKE_PICTURE_REQUEST_CODE = 1
+    val simpleDateFormat = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US)
     private lateinit var user: User
     private lateinit var pendingUser: User
     private lateinit var auth: FirebaseAuth
     private lateinit var database: DatabaseReference
+    private lateinit var storage: StorageReference
+    private lateinit var imageUri: Uri
 
     companion object {
         fun newInstance(): EditProfileFragment {
@@ -42,8 +57,8 @@ class EditProfileFragment : Fragment(), PasswordDialog.Listener {
         return inflater.inflate(R.layout.fragment_edit_profile, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         close_image.setOnClickListener {
             fragmentManager?.popBackStack()
@@ -52,11 +67,66 @@ class EditProfileFragment : Fragment(), PasswordDialog.Listener {
         save_image.setOnClickListener {
             updateProfile()
         }
+
+        change_photo_text.setOnClickListener {
+            takeCameraPicture()
+        }
+    }
+
+    private fun takeCameraPicture() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (intent.resolveActivity(context!!.packageManager) != null) {
+            val imageFile = createImageFile()
+            imageUri = FileProvider.getUriForFile(
+                context!!,
+                "com.github.rkhusainov.instagram.fileprovider", imageFile
+            )
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+            startActivityForResult(intent, TAKE_PICTURE_REQUEST_CODE)
+        }
+    }
+
+    private fun createImageFile(): File {
+        val storageDir = context!!.getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
+        return File.createTempFile(
+            "JPEG_${simpleDateFormat.format(Date())}_",
+            ".jpg",
+            storageDir
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == TAKE_PICTURE_REQUEST_CODE && resultCode == RESULT_OK) {
+            val uid = auth.currentUser!!.uid
+            storage.child("users/$uid/photo").putFile(imageUri).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    val downloadTask = it.result!!.metadata!!.reference!!.downloadUrl
+                    downloadTask.addOnSuccessListener { uri ->
+                        database.child("users/$uid/photo").setValue(uri.toString())
+                            .addOnCompleteListener { task ->
+                                if (task.isSuccessful) {
+                                    Log.d(TAG, "OnActivityResult: photo saved successfully")
+                                } else {
+                                    Toast.makeText(
+                                        context,
+                                        it.exception!!.message,
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                            }
+                    }
+                } else {
+                    Toast.makeText(context, it.exception!!.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
     }
 
     private fun getDataFromFirebase() {
         auth = FirebaseAuth.getInstance()
         database = FirebaseDatabase.getInstance().reference
+        storage = FirebaseStorage.getInstance().reference
         database.child("users").child(auth.currentUser!!.uid)
             .addListenerForSingleValueEvent(object : ValueEventListener {
                 override fun onDataChange(data: DataSnapshot) {
